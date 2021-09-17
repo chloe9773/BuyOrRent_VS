4da60a2056dfc315f45541950cfa31c073b72fe5
Chloe Park <jeonghwapark@outlook.com>
2021-09-17 13:30:27 +0900

    20210917_002 - 사용안하는 파일 제거 / common.css 내용 추가 / 중고거래 페이지 생성 및 작업 추가

--- common.js
+++ common.js
@@ -5,3 +5,20 @@ function itemDetail() {
 function toggleMenu() {
     $(".following-menu-drop").toggle();
 }
+
+function changeRegion(type, region) {
+    // alert(type);
+    // alert(region);
+    if(type == 'r1' && (region != '' || region != null)) {
+        $("#region2").removeAttr("disabled");
+    } 
+
+    if(type == 'r2') {
+        if (region.length > 0) {
+            $("#region3").removeClass('d-none');
+        } else if(region.length == 0) {
+            $("#region3").addClass('d-none');
+        }
+    } 
+
+}
