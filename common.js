e2a3f3df1ded68318234d4c35b03e8b425e0de6d
Chloe Park <jeonghwapark@outlook.com>
2021-09-24 17:52:24 +0900

    20210924_001 - 자유게시판 디테일뷰, 댓글창 추가

--- common.js
+++ common.js
@@ -22,3 +22,36 @@ function changeRegion(type, region) {
     } 
 
 }
+
+function fileUpload() {
+    $(".fileUpload").click();
+}
+
+function countChar() {
+    var text = $(".textarea").val();
+    var count = text.length;
+
+    $(".comment-total").html(count);
+}
+
+$(document).on('keyup', '.textarea', function(e){
+    var textarea = $(this).val();
+    var count = textarea.length;
+    $(".comment-total").html(count);
+});
+
+function replyCommentOpen(classNum) {
+    $(".comment-edit-" + classNum).removeClass("d-none");
+    $(".comment-reply-close-" + classNum).removeClass("d-none");
+    $(".comment-reply-open-" + classNum).addClass("d-none");
+}
+
+function replyCommentClose(classNum) {
+    $(".comment-edit-" + classNum).addClass("d-none");
+    $(".comment-reply-close-" + classNum).addClass("d-none");
+    $(".comment-reply-open-" + classNum).removeClass("d-none");
+}
+
+function optToggle(classNum) {
+    $(".opt-" + classNum).toggle();
+}
